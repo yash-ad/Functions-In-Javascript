c8d8c76d87677629dce172467a4375c8720a8d26
Yashraj Dandnaik <102809742+yash-ad@users.noreply.github.com>
2023-05-10 09:50:59 +0530

    Functions with objects and array in javascript with examples

--- Functions with objects and array.js
+++ Functions with objects and array.js
@@ -1,1 +1,17 @@
-//Functions with objects and array in javascrip
+//Functions with objects and array in javascript
+
+
+//1.addToCart example using function and spread operator:-
+
+function addToCart(...setValue) //spread operator.
+{
+
+return setValue;
+
+}
+
+const getValue = addToCart(200,400,500,600,700,800,900);
+console.log(getValue); //(7) [200, 400, 500, 600, 700, 800, 900]
+
+
+//
