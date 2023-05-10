24ce4e4536b25ee19f69cc30aa11f089e5257778
Yashraj Dandnaik <102809742+yash-ad@users.noreply.github.com>
2023-05-10 12:49:20 +0530

    Scope level and mini hoisting in javascript

--- Global and Local Scope.js
+++ Global and Local Scope.js
@@ -27,3 +27,37 @@ let a = 100;
 console.log(c)     //30 because its a global variable , best practice is to avoid var keyword.
 
 console.log(`Outer Value which is Global scope :${a}`); // 100   let a in block scope and let a in global scope and local scope , their values are totally different and independent from each other.
+
+
+
+
+//Scope level and mini hoisting in javascript:-
+//NestedScope :-
+
+
+
+function fnOne() {
+const userName = "Yash";
+
+
+    function fnTwo(){
+
+     const webSite = "LinkedIn"
+     console.log(webSite);
+      console.log(userName);
+    }
+
+    //console.log(webSite); //website is not defined ,It will caught an error because we have accessed outer of a scope.so we cant accessed rest of executions since execution starts line by line.
+
+    fnTwo(); // LinkedIn and Yash 
+}
+
+fnOne();
+
+
+
+
+
+
+
+
